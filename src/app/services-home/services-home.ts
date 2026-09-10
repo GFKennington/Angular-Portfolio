a697982b11b8d3a44ce9fe5e65d11f3fb8811878
gkennin <gkennin@wgu.edu>
2026-09-10 17:33:50 -0600

    Added html/css to Contact/Services Pages

---
+++ src/app/services-home/services-home.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-services-home',
+  styleUrl: './services-home.css',
+  templateUrl: './services-home.html',
+})
+export class ServicesHome {}
